e54984de57d5cdb183443f709a9eeefc07cf4862
Dat Vo <dat.cit241@gmail.com>
2022-07-08 08:49:30 +0700

    Add GFG2 class
    This class supports hashing the password using the SHA-256 algorithm

---
+++ src/main/java/com/services/GFG2.java
@@ -0,0 +1,46 @@
+package com.services;
+
+import java.math.*;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+
+/**
+ * Supports Hashing password
+ */
+public class GFG2 {
+    private static byte[] getSHA(String input) throws NoSuchAlgorithmException {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        // digest() method called
+        // to calculate message digest of an input
+        // and return array of byte
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static String toHexString(byte[] hash) {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, hash);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 64) {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
+    }
+
+    public static String hash(String input) {
+        String result = null;
+        try {
+            result = toHexString(getSHA(input));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
