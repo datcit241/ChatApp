ee3b48d073b82fb143d898ece83f74680d916ab7
Thien3t02 <thuan.nguyenthanh.cit19@eiu.edu.vn>
2022-07-28 14:52:53 +0700

    Add  userRepository, groupRepository, messageRepository, fileRepository and their setter getter to DataStorage.

--- src/main/java/com/data/DataStorage.java
+++ src/main/java/com/data/DataStorage.java
@@ -1,5 +1,8 @@
 package com.data;
 
+import com.models.conversations.groups.Group;
+import com.models.files.File;
+import com.models.messages.Message;
 import com.models.users.User;
 import com.repositories.Repository;
 
@@ -7,6 +10,16 @@ public class DataStorage {
     private static DataStorage storage;
 
     private static Repository<User> userRepository;
+    private static Repository<Group> groupRepository;
+    private static Repository<Message> messageRepository;
+    private static Repository<File> fileRepository;
+
+    static {
+        userRepository = new Repository<>();
+        groupRepository = new Repository<>();
+        messageRepository = new Repository<>();
+        fileRepository = new Repository<>();
+    }
 
     public DataStorage() {
 
@@ -19,4 +32,42 @@ public class DataStorage {
 
         return storage;
     }
+
+    public static void setStorage(DataStorage storage) {
+        DataStorage.storage = storage;
+    }
+
+    public static Repository<User> getUserRepository() {
+        return userRepository;
+    }
+
+    public static void setUserRepository(Repository<User> userRepository) {
+        DataStorage.userRepository = userRepository;
+    }
+
+    public static Repository<Group> getGroupRepository() {
+        return groupRepository;
+    }
+
+    public static void setGroupRepository(Repository<Group> groupRepository) {
+        DataStorage.groupRepository = groupRepository;
+    }
+
+    public static Repository<Message> getMessageRepository() {
+        return messageRepository;
+    }
+
+  
+    public static void setMessageRepository(Repository<Message> messageRepository) {
+        DataStorage.messageRepository = messageRepository;
+    }
+
+    public static Repository<File> getFileRepository() {
+        return fileRepository;
+    }
+
+    public static void setFileRepository(Repository<File> fileRepository) {
+        DataStorage.fileRepository = fileRepository;
+    }
+
 }
