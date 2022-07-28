ba82b93b5110c3d1db19326bab740e90ac771ab6
Thien3t02 <thuan.nguyenthanh.cit19@eiu.edu.vn>
2022-07-28 15:12:08 +0700

    Update DataStorage. Removing static key word from the Repositories

--- src/main/java/com/data/DataStorage.java
+++ src/main/java/com/data/DataStorage.java
@@ -9,19 +9,12 @@ import com.repositories.Repository;
 public class DataStorage {
     private static DataStorage storage;
 
-    private static Repository<User> userRepository;
-    private static Repository<Group> groupRepository;
-    private static Repository<Message> messageRepository;
-    private static Repository<File> fileRepository;
+    private Repository<User> userRepository;
+    private Repository<Group> groupRepository;
+    private Repository<Message> messageRepository;
+    private Repository<File> fileRepository;
 
-    static {
-        userRepository = new Repository<>();
-        groupRepository = new Repository<>();
-        messageRepository = new Repository<>();
-        fileRepository = new Repository<>();
-    }
-
-    public DataStorage() {
+    private DataStorage() {
 
     }
 
@@ -33,41 +26,44 @@ public class DataStorage {
         return storage;
     }
 
+    public static DataStorage getStorage() {
+        return storage;
+    }
+
     public static void setStorage(DataStorage storage) {
         DataStorage.storage = storage;
     }
 
-    public static Repository<User> getUserRepository() {
+    public Repository<User> getUserRepository() {
         return userRepository;
     }
 
-    public static void setUserRepository(Repository<User> userRepository) {
-        DataStorage.userRepository = userRepository;
+    public void setUserRepository(Repository<User> userRepository) {
+        this.userRepository = userRepository;
     }
 
-    public static Repository<Group> getGroupRepository() {
+    public Repository<Group> getGroupRepository() {
         return groupRepository;
     }
 
-    public static void setGroupRepository(Repository<Group> groupRepository) {
-        DataStorage.groupRepository = groupRepository;
+    public void setGroupRepository(Repository<Group> groupRepository) {
+        this.groupRepository = groupRepository;
     }
 
-    public static Repository<Message> getMessageRepository() {
+    public Repository<Message> getMessageRepository() {
         return messageRepository;
     }
 
-  
-    public static void setMessageRepository(Repository<Message> messageRepository) {
-        DataStorage.messageRepository = messageRepository;
+    public void setMessageRepository(Repository<Message> messageRepository) {
+        this.messageRepository = messageRepository;
     }
 
-    public static Repository<File> getFileRepository() {
+    public Repository<File> getFileRepository() {
         return fileRepository;
     }
 
-    public static void setFileRepository(Repository<File> fileRepository) {
-        DataStorage.fileRepository = fileRepository;
+    public void setFileRepository(Repository<File> fileRepository) {
+        this.fileRepository = fileRepository;
     }
 
 }
