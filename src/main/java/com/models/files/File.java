f4fe153df43f82d2e8e0a95d988b4a62f9bf2433
Dat Vo <dat.cit241@gmail.com>
2022-07-29 09:55:23 +0700

    Implement File.java

--- src/main/java/com/models/files/File.java
+++ src/main/java/com/models/files/File.java
@@ -1,7 +1,39 @@
 package com.models.files;
 
-/**
- * Type, extension, id, name
- */
+import com.models.enums.FileType;
+
 public class File {
+    public static String path;
+
+    private String id;
+    private FileType type;
+    private String extension;
+    private String name;
+
+    public File(FileType type, String id, String extension, String name) {
+        this.type = type;
+        this.id = id;
+        this.extension = extension;
+        this.name = name;
+    }
+
+    public static void setPath(String path) {
+        File.path = path;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public FileType getType() {
+        return type;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
