9c9b05323da552b11346a225a06dba18115c3a0d
Dat Vo <dat.cit241@gmail.com>
2022-07-29 13:50:28 +0700

    Add some methods to identify relation of a person or group to the message and a messageByRecentnessComparator

--- src/main/java/com/models/messages/Message.java
+++ src/main/java/com/models/messages/Message.java
@@ -1,9 +1,12 @@
 package com.models.messages;
 
+import com.models.enums.RelationToAMessage;
 import com.models.files.File;
 import com.models.users.User;
 
-import java.time.LocalDateTime;
+import javax.servlet.http.Part;
+import java.time.*;
+import java.util.Comparator;
 
 /**
  * Represents a message in a conversation
@@ -11,17 +14,75 @@ import java.time.LocalDateTime;
  * {Type: can be texts, images or files}
  */
 public class Message {
+    public static Comparator messageByRecentnessComparator;
+
+    static {
+        messageByRecentnessComparator = (o1, o2) -> -((Message) o1).getSentAt().compareTo(((Message) o2).getSentAt());
+    }
+
     private LocalDateTime sentAt;
     private User sender;
-    private User receiver;
+    private Object receiver;
     private String textContent;
     private File file;
 
-    public Message(User sender, User receiver, String text, File file) {
+    public Message(User sender, User receiver, String text, Part filePart) {
         this.sender = sender;
         this.receiver = receiver;
         this.textContent = text;
         this.sentAt = LocalDateTime.now();
-        this.file = file;
+
+
+    }
+
+    private File createFile(Part out) {
+        return new File(null, null, null, null);
+    }
+
+    public RelationToAMessage getRelation(Object conversationEntity) {
+        boolean isSender = conversationEntity.equals(sender);
+        boolean isReceiver = conversationEntity.equals(receiver);
+
+        if (!isSender && !isReceiver) {
+            return RelationToAMessage.NotRelated;
+        }
+
+        if (isSender) {
+            return RelationToAMessage.Sender;
+        }
+
+        return RelationToAMessage.Receiver;
+    }
+
+    public boolean isRelatedTo(Object conversationEntity) {
+        if (this.getRelation(conversationEntity) == RelationToAMessage.NotRelated) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isRelatedTo(Object conversationEntity1, Object conversationEntity2) {
+        return this.isRelatedTo(conversationEntity1) && this.isRelatedTo(conversationEntity2);
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public Object getReceiver() {
+        return receiver;
+    }
+
+    public String getTextContent() {
+        return textContent;
+    }
+
+    public File getFile() {
+        return file;
     }
 }
