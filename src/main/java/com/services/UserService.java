9eb76a3699bc9e5220f36c65606c7537b9fa2ce8
Dat Vo <dat.cit241@gmail.com>
2022-07-29 13:51:52 +0700

    Add getLatestMessages() and getMessagesContainingKeyword() to UserService.java

--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -2,9 +2,15 @@ package com.services;
 
 import com.data.DataStorage;
 import com.models.enums.LoginStatus;
+import com.models.messages.Message;
 import com.models.users.User;
 import com.utilities.HashHelper;
 
+import java.util.Collection;
+
+/**
+ * Should add sendMessage()
+ */
 public class UserService {
     DataStorage dataStorage = DataStorage.getDataStorage();
 
@@ -26,5 +32,29 @@ public class UserService {
         return LoginStatus.Successfully;
     }
 
+    public Iterable<Message> getLatestMessages(User user, Object conversationEntity, int k, int m) {
+        final int[] count = {0};
+
+        Iterable<Message> messagesInReverse = dataStorage
+                .getMessageRepository()
+                .get(message -> message.isRelatedTo(user, conversationEntity), Message.messageByRecentnessComparator);
+
+        Iterable<Message> messagesInRange = ((Collection<Message>) messagesInReverse)
+                .stream()
+                .filter(message -> count[0] >= m && count[0]++ <= k)
+                .toList();
+
+        return messagesInRange;
+    }
+
+    public Iterable<Message> getMessagesContainingKeyword(User user, Object conversationEntity, String keyword) {
+        Iterable<Message> messagesContainingKeyword = dataStorage
+                .getMessageRepository()
+                .get(message -> message.isRelatedTo(user, conversationEntity) && message.getTextContent().toLowerCase().contains(keyword.toLowerCase())
+                        , Message.messageByRecentnessComparator
+                );
+
+        return messagesContainingKeyword;
+    }
 
 }
