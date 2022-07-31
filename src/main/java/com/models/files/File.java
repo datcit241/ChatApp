f13335fad1ff662dbfaab69a9c8e0275db828d7e
Dat Vo <dat.cit241@gmail.com>
2022-07-31 15:17:41 +0700

    Add FileService.java

--- src/main/java/com/models/files/File.java
+++ src/main/java/com/models/files/File.java
@@ -3,22 +3,17 @@ package com.models.files;
 import com.models.enums.FileType;
 
 public class File {
-    public static String path;
-
     private String id;
     private FileType type;
     private String extension;
     private String name;
+
 
     public File(FileType type, String id, String extension, String name) {
         this.type = type;
         this.id = id;
         this.extension = extension;
         this.name = name;
-    }
-
-    public static void setPath(String path) {
-        File.path = path;
     }
 
     public String getId() {
---
+++ src/main/java/com/services/FileService.java
@@ -0,0 +1,114 @@
+package com.services;
+
+import com.data.DataStorage;
+import com.models.enums.FileType;
+
+import javax.servlet.http.*;
+import java.io.*;
+
+public class FileService {
+    private static String path;
+    private DataStorage dataStorage;
+
+    static {
+        path = "resources";
+    }
+
+    public FileService() {
+        dataStorage = DataStorage.getDataStorage();
+    }
+
+    public static void setPath(String path) {
+        FileService.path = path;
+    }
+
+    public void createFile(FileType fileType, String id, Part filePart) throws IOException {
+        String name = getFileName(filePart);
+        String extension = "";
+
+        try {
+            extension = name.substring(name.lastIndexOf(".") + 1);
+        } catch (Exception e) {
+
+        }
+
+        createPhysicalFile(id, filePart);
+
+        com.models.files.File file = new com.models.files.File(fileType, id, extension, name);
+        dataStorage.getFileRepository().insert(file);
+    }
+
+    public void createPhysicalFile(String id, Part filePart) throws IOException {
+        String fileName = id;
+
+        OutputStream out = null;
+        InputStream filecontent = null;
+
+        try {
+            out = new FileOutputStream(new java.io.File(path + java.io.File.separator + fileName));
+            filecontent = filePart.getInputStream();
+
+            int read = 0;
+            final byte[] bytes = new byte[1024];
+
+            while ((read = filecontent.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+        } catch (FileNotFoundException fne) {
+            fne.printStackTrace();
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+            if (filecontent != null) {
+                filecontent.close();
+            }
+        }
+    }
+
+    private String getFileName(final Part part) {
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+
+    public com.models.files.File getFileWithId(String id) {
+        com.models.files.File match = dataStorage.getFileRepository().find(file -> file.getId().equals(id));
+
+        return match;
+    }
+
+    public com.models.files.File getFileWithExtension(String extension) {
+        com.models.files.File match = dataStorage.getFileRepository().find(file -> file.getExtension().equals(extension));
+
+        return match;
+    }
+
+    public boolean removeFile(String id) {
+        com.models.files.File file = getFileWithId(id);
+
+        boolean physicalFileRemoved = removePhysicalFile(id);
+
+        if (file == null || physicalFileRemoved) {
+            return false;
+        }
+
+        dataStorage.getFileRepository().delete(file);
+        return true;
+    }
+
+    public boolean removePhysicalFile(String id) {
+        File file = new File(path + File.separator + id);
+
+        try {
+            return file.delete();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+}
