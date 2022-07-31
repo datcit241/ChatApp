41111f08198c487c2aafe06e485c535ebc9491b4
Dat Vo <dat.cit241@gmail.com>
2022-07-31 12:44:35 +0700

    Add addMember() into GroupService.java

--- src/main/java/com/services/GroupService.java
+++ src/main/java/com/services/GroupService.java
@@ -1,15 +1,24 @@
 package com.services;
 
 import com.data.DataStorage;
-import com.models.enums.RelationToAMessage;
 import com.models.files.File;
 import com.models.groups.Group;
 import com.models.messages.Message;
+import com.models.users.User;
 
 import java.util.*;
 
 public class GroupService {
     private DataStorage dataStorage = DataStorage.getDataStorage();
+
+    public boolean addMember(User member, Group group) {
+        if (group.hasParticipant(member)) {
+            return false;
+        }
+
+        group.addParticipant(member);
+        return true;
+    }
 
     public Iterable <File> getAllFiles(Group group) {
         Iterable<Message> messagesContainingFiles = dataStorage
