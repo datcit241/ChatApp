1ec1e01a548bce5576617dd3f069f33bab3be8b4
Thien3t02 <thuan.nguyenthanh.cit19@eiu.edu.vn>
2022-08-01 15:05:58 +0700

    Initial commit of tests

--- src/test/java/com/services/PublicGroupServiceTest.java
+++ src/test/java/com/services/PublicGroupServiceTest.java
@@ -1,12 +1,33 @@
 package com.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.data.DataStorage;
+import com.enums.Gender;
+import com.models.users.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 class PublicGroupServiceTest {
 
-    @Test
-    void joinWithAccessCode() {
-    }
+	private PublicGroupService pbGroupService;
+	private DataStorage dataStorage;
+	
+	@BeforeEach
+	void setUp() {
+		User userTest = new User("Spardon", "19331Ntt#", "Hau", "Nguyen", Gender.Male, LocalDate.of(2001, 3, 8));
+		String accessCode = "EMA_BJZ";
+		
+	}
+
+	@Test
+	void joinWithAccessCode() {
+		
+		Boolean expected = true;
+		
+		
+	}
 }
--- src/test/java/com/services/UserServiceTest.java
+++ src/test/java/com/services/UserServiceTest.java
@@ -2,63 +2,114 @@ package com.services;
 
 import org.junit.jupiter.api.Test;
 
+import com.data.DataStorage;
+import com.enums.Gender;
+import com.models.users.User;
+import com.repositories.Repository;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+
 class UserServiceTest {
+	private DataStorage dataStorage = DataStorage.getDataStorage();
+	private UserService userService = new UserService();
+	private Repository<User> userRepository;
+	static User user1;
+	static User user1Friend;
+	static User user2;
 
-    @Test
-    void addUser() {
-    }
+	@BeforeEach
+	void setUp() throws Exception {
+		userRepository = new Repository<>();
+		user1 = new User("Spardon", "19331Ntt#", "Huan", "Nguyen", Gender.Male, LocalDate.of(2001, 3, 8));
+		user1Friend = new User("DatCit", "24112001", "Tien Dat", "Vo Nguyen ", Gender.Male, LocalDate.of(2001, 11, 24));
+		user2 = new User("HuanGia", "23052001cit", "Nguyen", "Trung", Gender.Female, LocalDate.of(2001, 05, 23));
 
-    @Test
-    void login() {
-    }
+		dataStorage.getUserRepository().insert(user1);
+		
+		
 
-    @Test
-    void addFriend() {
-    }
+	}
 
-    @Test
-    void sendMessage() {
-    }
+	@Test
+	void addUser_ShouldnotAddUser() {
+		Boolean expected = false;
+		Boolean actual = userService.addUser("Spardon", "19331Ntt#", "Hau", "Nguyen", Gender.Male,
+				LocalDate.of(2001, 3, 8));
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    void removeMessage() {
-    }
+	@Test
+	void addUser_ShouldAddUser() {
+		Boolean expected = true;
+		Boolean actual = userService.addUser("RedRibbon", "19331Ntt#", "Nghia", "Nguyen", Gender.Male,
+				LocalDate.of(2001, 3, 8));
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    void getFriends() {
-    }
+	@Test
+	void login() {
+	}
 
-    @Test
-    void getLatestMessages() {
-    }
+	@Test
+	void addFriend_ShouldAddFriend() {
+		boolean actual = userService.addFriend(user1, user2);
+		boolean expected = true;
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    void getMessagesContainingKeyword() {
-    }
+	@Test
+	void addFriend_ShouldNotAddFriend() {
+		boolean actual = userService.addFriend(user1, user2);
+		boolean expected = false;
+		assertEquals(expected, actual);
 
-    @Test
-    void getIdsOfRelatedConversationEntities() {
-    }
+	}
 
-    @Test
-    void getJoinedGroups() {
-    }
+	@Test
+	void sendMessage() {
+	}
 
-    @Test
-    void getContacts() {
-    }
+	@Test
+	void removeMessage() {
+	}
 
-    @Test
-    void leaveGroup() {
-    }
+	@Test
+	void getFriends() {
+	}
 
-    @Test
-    void setAlias() {
-    }
+	@Test
+	void getLatestMessages() {
+	}
 
-    @Test
-    void getFriendName() {
-    }
+	@Test
+	void getMessagesContainingKeyword() {
+	}
+
+	@Test
+	void getIdsOfRelatedConversationEntities() {
+	}
+
+	@Test
+	void getJoinedGroups() {
+	}
+
+	@Test
+	void getContacts() {
+	}
+
+	@Test
+	void leaveGroup() {
+	}
+
+	@Test
+	void setAlias() {
+	}
+
+	@Test
+	void getFriendName() {
+	}
 }
